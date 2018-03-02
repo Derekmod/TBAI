444a69a654e21fa42ea7de31a685889a95943e3f
Derekmod <derekmod@gmail.com>
2018-03-02 03:57:20 -0500

    quick fix

--- python/TBAI/TBAI.py
+++ python/TBAI/TBAI.py
@@ -4,15 +4,14 @@ from ai import AIPlayer
 
 from tictactoe_ai import TicTacToeNet
 
-game = TicTacToeGame()
-
 model = TicTacToeNet()
 player1 = AIPlayer(0, lambda x: x, model=model)
 #player1 = Player()
 #player2 = Player()
 player2 = HumanTicTacToePlayer()
 
-player1.key = game.registerPlayer(player1)
-player2.key = game.registerPlayer(player2)
+for _ in range(5):
+    game = TicTacToeGame()
 
-game.start()
+    player1.key = game.registerPlayer(player1)
+    player2.key = game.registerPlayer(player2)
