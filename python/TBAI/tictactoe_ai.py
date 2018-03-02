df0b8e6519681af7b1950ab9e04e5810772d532f
Derekmod <derekmod@gmail.com>
2018-03-01 22:47:16 -0500

    quick fix

--- python/TBAI/tictactoe_ai.py
+++ python/TBAI/tictactoe_ai.py
@@ -9,7 +9,7 @@ class TicTacToeNet(nn.Module):
     def __init__(self):
         super(TicTacToeNet, self).__init__()
         self.fc1 = nn.Linear(10, 50)
-        self.fc2 = nn.Linear(51, 20)
+        self.fc2 = nn.Linear(5, 20)
         self.fc3 = nn.Linear(21, 2)
         
         self.fc_out = nn.Linear(1+9+50+20, 2)
@@ -17,8 +17,8 @@ class TicTacToeNet(nn.Module):
     def forward(self, x):
         turn = x[0:1]
         x1 = F.relu(self.fc1(x))
-        x2 = F.relu(self.fc2(turn + x1))
-        y = F.sigmoid(self.fc3(turn + x2))
+        x2 = F.relu(self.fc2(x1))
+        y = F.sigmoid(self.fc3(x2))
         
         # y = F.sigmoid(self.fc_out(turn + x + x1 + x2))
         
