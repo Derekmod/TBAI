bdd087457013c62c12db2d7039cdda5a75e4d674
Derekmod <derekmod@gmail.com>
2018-05-03 00:53:40 -0400

    update game activation

--- python/TBAI/tbai_nn.py
+++ python/TBAI/tbai_nn.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import torch
 
 
@@ -5,7 +7,12 @@ def gameActivation(input):
     # break into v and u
     # apply sigmoid to v
     # apply softmax to u
-    left, right = input.split(2, 1)
+
+    splits = input.split(2,1)
+    print(splits)
+    #left, right = input.split(2, 1)
+
+    left, right = splits[0], splits[1]
 
     value = torch.sigmoid(left)
     uncertainty = torch.softplus(right)
